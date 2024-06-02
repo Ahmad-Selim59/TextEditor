9d01091992808221d63fdfc9bdfeef239dcd0824
Ahmad-Selim59 <ahmad22selim@gmail.com>
2024-06-02 19:48:40 +0100

    added text area, menu bar with options and scroll bars

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,20 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class GUI {
 
     JFrame window;
-
+    JTextArea area;
+    JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu fileMenu, editMenu, formatMenu, colourMenu;
     public static void main(String[] args){
         new GUI();
     }
 
     public GUI(){
         createWindow();
+        createTextArea();
+        createMenuBar();
         window.setVisible(true);
     }
 
@@ -17,6 +22,31 @@ public class GUI {
         window = new JFrame("Notepad");
         window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void createTextArea(){
+        area = new JTextArea();
+        scrollPane = new JScrollPane(area, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        window.add(scrollPane);
+
+    }
+
+    public void createMenuBar(){
+        menuBar = new JMenuBar();
+
+        window.setJMenuBar(menuBar);
+
+        fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+
+        editMenu = new JMenu("Edit");
+        menuBar.add(editMenu);
+
+        formatMenu = new JMenu("Format");
+        menuBar.add(formatMenu);
+
+        colourMenu = new JMenu("Colour");
+        menuBar.add(colourMenu);
 
     }
 
